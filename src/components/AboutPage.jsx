bebf5ce70894a89aa9e0a4013518180597aa5f1b
MASA-JAPAN <masaifactoryline@gmail.com>
2018-12-29 01:46:24 +0900

    Commit

---
+++ src/components/AboutPage.jsx
@@ -0,0 +1,10 @@
+import React  from 'react';
+
+const AboutPaget =()=> (
+  <div className="about">
+    <h1 className="app-title">このサイトについて</h1>
+  </div>
+);
+
+
+export default AboutPaget;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,15 +7,21 @@ import {
 } from 'react-router-dom';
 
 import SearchPage from './SearchPage';
+import AboutPage from './AboutPage';
 
 const App =()=> (
+  <Router>
   <div className="App">
     <ul className='left-navi'>
-      <li><a href="/">ホテル検索</a></li>
-      <li><a href="/about">About</a></li>
+      <li><Link to="/">ホテル検索</Link></li>
+      <li><Link to="/about">About</Link></li>
     </ul>
-    <SearchPage/>
+    <Switch>
+      <Route exact path="/" component = {SearchPage}/>
+      <Route exact path ="/about" component = {AboutPage}/>
+    </Switch>
   </div>
+  </Router>
 );
 
 
